fa01f6825bd63771dfda245e38ccf4f128b95e37
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-08-23 11:09:58 +0100

    Create pg188.py

---
+++ Chapter 9/pg188.py
@@ -0,0 +1,6 @@
+carAge = int(input("Enter age of car: "))
+carFuel = input("Enter car's fuel type (petrol/diesel): ")
+if carAge >=10 OR carFuel =="diesel":
+  print("Pollution level high")
+else:
+  print("Pollution level low")
