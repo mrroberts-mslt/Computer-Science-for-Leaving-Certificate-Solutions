6228da92ecd6b90ccb9e66afdc89c18db21e7789
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-08-23 10:11:15 +0100

    Update and rename pg20_encription.py to pg20_encryption.py

--- Chapter 1/pg20_encription.py
+++
@@ -1,5 +0,0 @@
-character = "C"
-key=3
-ascii = ord(character)
-encriptedLetter = chr(ascii + key)
-print("The encripted letter is: ", encriptedLetter)
---
+++ Chapter 1/pg20_encryption.py
@@ -0,0 +1,5 @@
+character = "C"
+key=3
+ascii = ord(character)
+encryptedLetter = chr(ascii + key)
+print("The encrypted letter is: ", encryptedLetter)
