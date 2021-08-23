841cf4801f8efa050e62ce30d477414cdf207046
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-08-23 11:04:05 +0100

    Update pg187.py

--- Chapter 9/pg187.py
+++ Chapter 9/pg187.py
@@ -1,1 +1,11 @@
-
+age = int(input("Please enter your age:"))
+hasProvisional = input("do you have a provisional licence? (y/n)? ")
+#First outer if statement
+if age >= 17:
+  #inner if statement
+  if hasProvisional == "y":
+    print("You are eligible to apply for a Driving Licence.")
+  else:
+    print("You are not eligible to apply for a driving licence. You must get a provisional licence first.")
+else:
+  print("You are not eligible to apply for a driving licence. You must be 17 first")
