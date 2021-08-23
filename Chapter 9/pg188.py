755ecda12114e9a92dee65e8cc4a2a9887678f7d
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-08-23 11:12:45 +0100

    Update pg188.py

--- Chapter 9/pg188.py
+++ Chapter 9/pg188.py
@@ -1,6 +1,7 @@
 carAge = int(input("Enter age of car: "))
 carFuel = input("Enter car's fuel type (petrol/diesel): ")
-if carAge >=10 OR carFuel =="diesel":
+#note boolean needs to be lowercase
+if carAge >=10 or carFuel =="diesel":
   print("Pollution level high")
 else:
   print("Pollution level low")
