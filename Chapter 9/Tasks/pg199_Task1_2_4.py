1765958ba57097d7c6c36351e147be7aa904df6c
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-08-26 10:26:03 +0100

    Update pg199_Task1_2_4.py

--- Chapter 9/Tasks/pg199_Task1_2_4.py
+++ Chapter 9/Tasks/pg199_Task1_2_4.py
@@ -13,7 +13,24 @@ piVal()
 
 #task4
 #An argument is the value that is sent to the function when it is called.
-def myFunction(baseNum, indexNum):
+def myFunction1(baseNum, indexNum):
   print(baseNum ** indexNum)
 
-myFunction(5, 6) 
+myFunction1(5, 6) 
+
+#task5
+#An argument is the value that is sent to the function when it is called.
+
+
+def myFunction2(num1, num2):
+  if num1 > num2:
+    print(num1, "is larger")
+  elif:
+    num2 > num1:
+    print(num2, "is larger")
+  else:
+    print(num1, " and ", num2 "are the same")
+    
+num1 = int(input("enter number 1: "))
+num2 = int(input("enter number 2: "))
+myFunction2(x, y) 
