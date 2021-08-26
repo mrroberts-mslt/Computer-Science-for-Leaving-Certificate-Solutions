f5c492fdd6ff18509e90f41e6211d6bdfa4e896e
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-08-26 10:31:15 +0100

    Update and rename pg199_Task1_2_4.py to pg199_Task1_2_4_5.py

--- Chapter 9/Tasks/pg199_Task1_2_4_5.py
+++ Chapter 9/Tasks/pg199_Task1_2_4_5.py
@@ -22,15 +22,17 @@ myFunction1(5, 6)
 #An argument is the value that is sent to the function when it is called.
 
 
+#task5
 def myFunction2(num1, num2):
   if num1 > num2:
     print(num1, "is larger")
-  elif:
-    num2 > num1:
+  
+  elif num2 > num1:
     print(num2, "is larger")
+  
   else:
-    print(num1, " and ", num2 "are the same")
+    print(num1, " and ", num2, "are the same")
     
 num1 = int(input("enter number 1: "))
 num2 = int(input("enter number 2: "))
-myFunction2(x, y) 
+myFunction2(num1, num2) 
