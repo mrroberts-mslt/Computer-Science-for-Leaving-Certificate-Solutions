915be013731ec4f0daa3d7039902f528cec74e8b
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-08-27 11:21:49 +0100

    Update pg204_UnitTesting.py

--- Chapter 9/pg204_UnitTesting.py
+++ Chapter 9/pg204_UnitTesting.py
@@ -16,3 +16,29 @@ def unitTest():
     passed = False
   return passed
 print("Test passed:", unitTest())
+
+def largerNumber(num1,num2):
+  if num1 > num2:
+    return num1
+  else:
+    return num2
+  
+def unitTest():
+  firstNums = [1,2,3,4,5]
+  secondNums = [1,1,4,-1,100]
+  
+  fails = 0
+  for i in range(len(firstNums)):
+    #[i] is the loop position value from the list
+    test1 = firstNums[i]
+    test2 = secondNums[i]
+    testAns = largerNumber(test1,test2)
+    if test1 > test2:
+      if test1 != testAns:
+        fails +=1
+     else:
+			if test2 != testAns:
+				fails +=1
+		return fails
+	print("Total Fails: ", unitTest())
+    
