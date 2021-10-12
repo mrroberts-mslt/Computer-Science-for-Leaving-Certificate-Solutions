9d024b050b70a030f2dc0e3d86a3a800c43a0904
Huw Roberts <hproberts@gmail.com>
2021-10-12 12:15:11 +0100

    update

--- Chapter 9/pg214_Sorting.py
+++ Chapter 9/pg214_Sorting.py
@@ -1,3 +1,4 @@
+#run this code and add your own comments to explain what is happening in each line
 myList = [85,24,63,45,17,31,96,50]
 for index in range(len(myList)-1):
   print (myList)
@@ -7,4 +8,3 @@ for index in range(len(myList)-1):
       myList[nextMinIndex] = myList[index]
       myList[index]= nextMinValue
 print(myList)
-hello
