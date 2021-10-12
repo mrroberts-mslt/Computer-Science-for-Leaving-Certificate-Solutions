86701581419b57777ca5d02369df6194e74de06a
Huw Roberts <hproberts@gmail.com>
2021-10-12 12:01:52 +0100

    UPDATED ON ATOM

--- Chapter 9/pg214_Sorting.py
+++ Chapter 9/pg214_Sorting.py
@@ -2,5 +2,8 @@ myList = [85,24,63,45,17,31,96,50]
 for index in range(len(myList)-1):
   print (myList)
   nextMinValue = min(myList[index + 1:])
-
-
+  if nextMinValue < myList[index]:
+      nextMinIndex = myList.index(nextMinValue)
+      myList[nextMinIndex] = myList[index]
+      myList[index]= nextMinValue
+print(myList)
