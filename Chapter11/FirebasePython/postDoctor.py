030be56c60201af98f483cb29eda408461912e44
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2021-11-16 12:53:40 +0000

    Create postDoctor.py

---
+++ Chapter11/FirebasePython/postDoctor.py
@@ -0,0 +1,10 @@
+from firebase import firebase
+
+myDB = firebase.FirebaseApplication("https://drpatients-68bc6-default-rtdb.firebaseio.com/", None )
+
+record = {
+    "docFN" :   "David",
+    "docLN" :   "Doolittle"
+}
+
+myDB.post("doctors",record)
