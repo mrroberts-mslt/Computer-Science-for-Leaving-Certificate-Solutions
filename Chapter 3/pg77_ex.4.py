a71ad1340191ae50d66e671d3483a7ff01932f46
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2022-04-05 18:06:32 +0100

    Update pg77_ex.4.py

--- Chapter 3/pg77_ex.4.py
+++ Chapter 3/pg77_ex.4.py
@@ -1,5 +1,14 @@
+#basic no loop method 
 myNums = (input("Please enter 10 number separated by commas: "))
 file = open("myNumbers.csv", "w")
 file.write(myNums)
+    
+file.close()
+
+#with for loop!!
+file = open("numbers.csv", "w")
+for i in range (10):
+    myNums = input("Please enter a number: ")
+    file.write((myNums)+",")
     
 file.close()
