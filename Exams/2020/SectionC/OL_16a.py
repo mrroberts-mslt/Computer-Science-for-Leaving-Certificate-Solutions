c159b5496e76678091fff684004766a006801929
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2022-05-12 08:01:26 +0100

    Create OL_16a.py

---
+++ Exams/2020/SectionC/OL_16a.py
@@ -0,0 +1,13 @@
+# Question 16(a)
+# Examination Number:
+
+num1 = 9
+num2 = 5
+
+print('Do you want me to (a)dd or (s)ubtract?')
+choice = input()
+
+if choice == 'a':
+  print (num1 + num2)
+elif choice == 's':
+  print (num1 - num2)
