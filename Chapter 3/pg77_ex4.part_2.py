45f424b09fdc3b03f20bb514791c9058539dab24
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2023-01-19 11:16:49 +0000

    Update pg77_ex4.part_2.py

--- Chapter 3/pg77_ex4.part_2.py
+++ Chapter 3/pg77_ex4.part_2.py
@@ -1,4 +1,11 @@
-file = open("myNumbers.csv", "r")
+#Needs correcting or use list comprehension
+file = open("numbers.csv", "r")
 dataIn = file.read()
 file.close()
-print(dataIn) #printed as a string
+myList = dataIn.split(",")
+print (len(myList))
+myList = []
+for i in range(len(myList)):
+    i = float(i)
+    myList.append(i)
+print(myList)
