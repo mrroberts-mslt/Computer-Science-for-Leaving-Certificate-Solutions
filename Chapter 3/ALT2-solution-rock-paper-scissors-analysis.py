6b6ab02803ba267e7a013231c6af497554257725
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2023-02-01 12:46:06 +0000

    Update and rename ALT2-rock-paper-scissors-analysis.py to ALT2-solution-rock-paper-scissors-analysis.py

--- Chapter 3/ALT2-solution-rock-paper-scissors-analysis.py
+++ Chapter 3/ALT2-solution-rock-paper-scissors-analysis.py
@@ -1,3 +1,11 @@
+#ALT2 Sample solution 
+#Challenge convert the original RPS game to make it computer v computer then see which result is the most likely to win? R,P, S?
+#Key skills: Change the code to make user computer generated random choice. 
+#Use a for loop to run the game 100 times, 
+#Append results to a csv file, 
+#Read the CSV file into Python and do some statistics on the results 
+#original rps code from https://realpython.com/python-rock-paper-scissors/
+
 import random
 import statistics
 
@@ -7,15 +15,20 @@ for i in range(100):
     user_action = random.choice(possible_actions)
     computer_action = random.choice(possible_actions)
     print(f"\nUser chose {user_action}, computer chose {computer_action}.\n")
+
     if user_action == computer_action:
         print(f"Both players selected {user_action}. It's a tie!")
-        file.write("Draw,")       
+        file.write("Draw,")
+        
+        
     elif user_action == "rock":
         if computer_action == "scissors":
             print("Rock smashes scissors! You win!")
             file.write("Rock,")
+
         else:
             print("Paper covers rock! You lose.")
+            
     elif user_action == "paper":
         if computer_action == "rock":
             print("Paper covers rock! You win!")
@@ -26,6 +39,7 @@ for i in range(100):
         if computer_action == "paper":
             print("Scissors cuts paper! You win!")
             file.write("Scissors,")
+        
         else:
             print("Rock smashes scissors! You lose.")
     file.close()
@@ -33,6 +47,10 @@ for i in range(100):
 file = open("rps.csv","r")
 dataIn = file.read()
 rpsList = dataIn.split(",")
+mode = statistics.mode(rpsList)
+print(f"The most common result (mode) is: {mode}")
+freq = rpsList.count(mode)
+print(f"The number of {mode}s is: {freq}")
 rockCount = 0
 paperCount = 0
 scissorCount = 0
@@ -51,5 +69,3 @@ print("Paper = ", paperCount)
 print("Scissors = ", scissorCount)
 print("Draw = ", drawCount)
 print("Total plays: ",rockCount+paperCount+scissorCount+drawCount)
-
-
