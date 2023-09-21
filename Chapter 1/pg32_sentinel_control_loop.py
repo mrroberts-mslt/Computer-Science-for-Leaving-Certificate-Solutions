68e9989a22b8cecca35987c6911958caa5654f69
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2023-09-21 09:44:00 +0100

    Update pg32_sentinel_control_loop.py

--- Chapter 1/pg32_sentinel_control_loop.py
+++ Chapter 1/pg32_sentinel_control_loop.py
@@ -1,8 +1,13 @@
+#Change the values in the while loop to only allow marks between 50 and 99
+#Change the error message
 mark = float(input("Enter and exam mark between 0 and 100: "))
 while mark <0 or mark >100:
     print ("Error, mark outside range.")
     mark = float(input("Re-enter mark: "))
 print ("You entered", mark, "- well done!")
+
+#Change the code below to make another number to finish the loop
+# Can you change the code to make the letter X to finish?
 
 num=int(input("Enter a number, 0 to finish:"))
 
