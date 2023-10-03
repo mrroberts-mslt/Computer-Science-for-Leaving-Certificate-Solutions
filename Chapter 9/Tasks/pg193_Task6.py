f480631ca277a40a1e70d6c13ee9d31007ff67eb
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2023-10-03 14:28:04 +0100

    Update pg193_Task6.py

--- Chapter 9/Tasks/pg193_Task6.py
+++ Chapter 9/Tasks/pg193_Task6.py
@@ -1,26 +1,28 @@
 #Aim is to produce a table of values giving price per square metre.
-price = 45.20
+
+price = float(input("Enter Price per sq m: "))
 print("\tWidth")
+
+# Print the table header
 # lets make the the top part of the table, last value in the range is the step e.g increase by 5
-for tableHead in range(0,25, 5):
-  #using a conditional to put Length in "cell a1"!
-  if tableHead == 0:
-    print("Length\t", end="")
-  else:
-    # had to fiddle around with tabs \t to get the spacing
-    print(tableHead,"m\t\t", end="")
+for tableHead in range(0, 25, 5):
+    if tableHead == 0:
+        print("Length\t", end="")
+    else:
+        print(tableHead, "m\t\t", end="")
 print()
 
-#this section does the calculation. 
+# Generate the table
 #first specify the range and increment 10m to 100m (110) with increment of 10
 for rowIndex in range(10, 110, 10):
-  # This is to do the multiplication part e.g 10*5
-  for colIndex in range(5,25, 5):
-    ans=price * (colIndex * rowIndex)
-    #make it sensible and readable
-    ans=round(ans,2)
-    print(rowIndex,"m\t", ans, end="")
-  print()
+    # Print the rowIndex in the first column and add an "m"
+    print(rowIndex,"m", "\t", end="")
+      # This is to do the multiplication part e.g 10*5
+    for colIndex in range(5, 25, 5):
+        ans = price * (colIndex * rowIndex)
+        ans = int(ans)
+        print("€",ans, end="\t\t")  # Separate other columns with double tabs
+    print()  # Start a new row
   
   
   
