63b3fcdf955d07cbb950515b6e7a5a510ab8d245
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2023-10-03 13:57:06 +0100

    Update pg192_Task4.py

--- Chapter 9/Tasks/pg192_Task4.py
+++ Chapter 9/Tasks/pg192_Task4.py
@@ -3,5 +3,19 @@ rowVal = int(input("Enter number of rows: "))
 numPrinted = 0
 
 while numPrinted <= rowVal:
-    print (numPrinted * "*")
+    print("*" * numPrinted)
     numPrinted += 1
+
+#Right Aligned
+
+
+rowVal = int(input("Enter number of rows: "))
+
+numPrinted = 1
+
+while numPrinted <= rowVal:
+    line = '*' * numPrinted  # Create a string of asterisks
+    print(line.rjust(rowVal))  # Right-align the string within a field of width 'rowVal'
+    numPrinted += 1
+
+
