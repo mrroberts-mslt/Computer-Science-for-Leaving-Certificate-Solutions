3974a51c87bda7b0e44324343302a42eaf116048
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-04-06 13:14:20 +0100

    Update pg215_InsertionSort.py

--- Chapter 9/pg215_InsertionSort.py
+++ Chapter 9/pg215_InsertionSort.py
@@ -1,1 +1,13 @@
 #=============INSERTION SORT================
+#EXPLANATION
+myList = [85,24,63,45,17,31,96,50]
+for i in range(1, len(myList)):
+  print(myList)
+  itemInsert = myList[i]
+  position = i
+  while poistion > 0 and myList[position-1] > itemInsert:
+    myList[position] = myList[position-1]
+    position -=1
+  myList[position] = itemInsert
+print(myList)
+  
