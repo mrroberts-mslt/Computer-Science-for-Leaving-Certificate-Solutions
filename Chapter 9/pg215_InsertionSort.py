4daa54196b59b8148d18c925005f8b99220770b9
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-04-06 13:37:10 +0100

    Update pg215_InsertionSort.py

--- Chapter 9/pg215_InsertionSort.py
+++ Chapter 9/pg215_InsertionSort.py
@@ -1,5 +1,11 @@
 #=============INSERTION SORT================
-#EXPLANATION
+#EXPLANATION look at the value next to index and move it to the left if its lower
+# Ignore the sorted items then move to the next value
+#Pass 1: 24(index1) is less than 85(index0) so its inserted into position 0
+# 24,85,63
+#Pass 2: so 85 > 63 and 24 > 63 False 
+# So insert 63 between the 2
+# 24,63,85
 myList = [85,24,63,45,17,31,96,50]
 for i in range(1, len(myList)):
   print(myList)
