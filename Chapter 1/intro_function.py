614393bdea9f3d0f9ec6acf25be81e770cf36c9e
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2024-10-24 08:41:50 +0000

    Create intro_function.py

---
+++ Chapter 1/intro_function.py
@@ -0,0 +1,14 @@
+#1. Can you work out what is happening here?
+print ("----AVERAGE SPEED CALCULATOR----")
+#2. Find out what does "def" mean ?
+def calculate(d,t):
+    speed=d/t
+    print("The average speed is:",speed,"m/s")
+    return
+
+distance=int(input("Enter the distance travelled in metres: "))
+time=int(input("Enter the time it took to complete the journey in seconds: "))
+#3. What is this line doing?
+calculate(distance,time)
+
+#4. Can you write your own function to calculate area?
