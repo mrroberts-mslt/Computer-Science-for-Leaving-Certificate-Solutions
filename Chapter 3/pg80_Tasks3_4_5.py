1b880f19620bc80fd3f3af55ba2ba37106cc1389
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-01-30 08:47:43 +0000

    Update pg80_Tasks3_4_5.py

--- Chapter 3/pg80_Tasks3_4_5.py
+++ Chapter 3/pg80_Tasks3_4_5.py
@@ -1,20 +1,21 @@
 import random
-#Task 1
+import csv
+
 # Open CSV file for writing
 file = open("myNums.csv", mode="w")
-
 
 # Generate and write 100 random integers between 0 and 100
 random_numbers = []
 for i in range(100):
     num = random.randint(0, 100)
     random_numbers.append(num)
-    file.write(str(num)+","+"\n")
+    file.write(str(num) + "\n")
 
 file.close()
+
 print("CSV file 'myNums.csv' has been created successfully.")
-#Task 2
-#Open new CSV file for numbers over 50
+
+# Open new CSV file for numbers over 50
 file = open("over50.csv", mode="w")
 counter = 0
 over_50 = []
@@ -33,23 +34,26 @@ for num in over_50:
 file.close()
 
 print(counter)
-print("CSV file 'over50.csv' has been created successfully.")
 
-#Task 3
-# Read myNums.csv, modify values greater than 30 to 30, and write to a new file
+# Read myNums.csv into a list
 infile = open("myNums.csv", mode="r")
-outfile = open("modifiedNums.csv", mode="w")
-reader = infile.readlines()
-
-for line in reader:
-    num = int(line.strip())
-    num = 30 if num > 30 else num
-    outfile.write(str(num) + "\n")
-
+dataIn = infile.readlines()
 infile.close()
+
+# Convert each item to an integer
+myList = []
+for item in dataIn:
+    myList.append(int(item.strip()))
+
+# Replace any item greater than 30 with 30
+for i in range(len(myList)):
+    if myList[i] > 30:
+        myList[i] = 30
+
+# Write modified values to new CSV file
+outfile = open("modifiedNums.csv", mode="w")
+for num in myList:
+    outfile.write(str(num) + "\n")
 outfile.close()
 
 print("CSV file 'modifiedNums.csv' has been created successfully.")
-
-
-
