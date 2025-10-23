3b4b268c9eb9893d4c21d19fffd81766923b9e52
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-10-23 08:14:09 +0000

    Update pg216_BubbleSort.py

--- Chapter 9/pg216_BubbleSort.py
+++ Chapter 9/pg216_BubbleSort.py
@@ -1,1 +1,10 @@
-myList = 
+myList = [85,24,63,45,17,31,96,50]
+for outerIndex in range(len(myList) - 1):
+  print(myList)
+  for index in range(len(myList) - 1):
+    if myList[index] > myList[index + 1]:
+      tempValue = myList[index]
+      myList[index] = myList[index+1]
+      myList[index+1] = tempValue
+  print(myList)
+  
