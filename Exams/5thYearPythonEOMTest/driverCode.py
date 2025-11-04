87552e3119c934fb660f3fbb9296bc4cc60eca5d
Huw Roberts <82210970+mrroberts-mslt@users.noreply.github.com>
2025-11-04 15:26:40 +0000

    Create driverCode.py

---
+++ Exams/5thYearPythonEOMTest/driverCode.py
@@ -0,0 +1,24 @@
+# --- STARTER CODE: modify below this line ---
+
+shopping_list = []
+
+print("Enter items for your shopping list.")
+print("Type 'remove' to delete an item, or 'done' to finish.")
+
+# TODO: use a while loop to keep asking for input
+# Inside the loop:
+#   - if the user types 'done', stop the loop
+#   - if the user types 'remove', ask for the item to remove
+#   - otherwise, add the item to the shopping_list
+
+# Example steps:
+# 1) item = input("Enter item: ")
+# 2) check what item equals
+# 3) use if / elif / else for the three cases above
+
+# After the loop:
+# - print the list of items
+# - print the number of items
+# - print a message saying whether "milk" is in the list
+
+# --- END STARTER CODE ---
